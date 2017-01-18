af6859e9fbfc36b36a94138e73c73e6801a7eb00
Bob Thomas <bob_mathias_thomas@hotmail.com>
2017-01-18 16:22:23 +0100

    GAME-46 State machine is working
    TODO:
       * add splash screen
       * add end screen
       * add pause screen

--- Source/GameStates/IState.cpp
+++
@@ -1,7 +0,0 @@
-//
-// Created by robbie on 17-1-2017.
-//
-
-#include "IState.h"
-
-
--- Source/main.cpp
+++ Source/main.cpp
@@ -6,23 +6,8 @@
 #include "Game.h"
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(800, 600), "WaveForms");
     // Game application class
     Game waveForms(window);
-
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-        // Game update method
-        waveForms.update();
-        // Game draw method
-        waveForms.draw();
-    }
-
     return 0;
 }
