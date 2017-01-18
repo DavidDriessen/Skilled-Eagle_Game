2f85bc2b0d3ca6a72b2f2681a7c42ff3391495c6
woodage <r-valkenburg@hotmail.com>
2017-01-18 18:01:14 +0100

    level factory is working

--- Source/GameStates/PlayState.cpp
+++ Source/GameStates/PlayState.cpp
@@ -9,25 +9,30 @@
 
 PlayState::PlayState(Game *pGame): level("assets/Levels/awesomeLevel.txt") {
     game = pGame;
-}
-
-PlayState::~PlayState() {
-
+    std::string mapName = "assets/Levels/awesomeLevel.txt";
+    level_objects = level.get_screen_objects_from_map(mapName);
 }
 
 void PlayState::input(sf::Event &event) {
     if(event.type == sf::Event::KeyPressed && sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
         game->go_to_menu();
     }
+    for(auto &object : level_objects) {
+        object->input(event);
+    }
 }
 
 void PlayState::update(const sf::Time delta) {
-
+    std::cout << "updating PlayState \n";
+    for(auto &object : level_objects) {
+        object->update(delta);
+    }
 }
 
 void PlayState::draw(sf::RenderWindow &window) {
-    for(auto &block : level.get_blocks()) {
-        block->draw(window);
+    std::cout << "drawing PlayState \n";
+    for(auto &object : level_objects) {
+        object->draw(window);
     }
 }
 
