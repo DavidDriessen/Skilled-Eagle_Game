1f027f243974db4d6e7bcfdfbc16e9f340dede8b
Max Beunk <mbeunk@live.nl>
2017-01-18 17:09:14 +0100

    Character class with jump and moving capabilities (main.cpp contains test values and code)

--- Source/Character.hpp
+++ Source/Character.hpp
@@ -11,6 +11,7 @@
 class Character {
     private:
         sf::Vector2f position;
+<<<<<<< HEAD
         sf::Image img;
         sf::Texture texture;
         sf::Sprite sprite;
@@ -18,12 +19,15 @@ class Character {
         float sprite_scale = 0.3;
         int sprite_height = 260;
         int sprite_width = 160;
+=======
+>>>>>>> Character class with jump and moving capabilities (main.cpp contains test values and code)
         float gravity;
         float startHeight;
         float jumpHeigth;
         float speed;
         bool jumping = false;
         bool descending = false;
+<<<<<<< HEAD
         void move(sf::Vector2f direction);
         void jump();
 
@@ -33,6 +37,16 @@ class Character {
         void update(const sf::Time delta);
         void input(sf::Event &event);
 
+=======
+
+    public:
+        Character(sf::Vector2f startPos, float gravity, float speed, float jumpHeight);
+        void move(sf::Vector2f direction);
+        void jump();
+        void update(sf::RenderWindow &w);
+        void setJumping(bool status);
+        float getSpeed();
+>>>>>>> Character class with jump and moving capabilities (main.cpp contains test values and code)
 };
 
 
