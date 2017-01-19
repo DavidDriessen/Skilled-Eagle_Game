9cd66b995dd4dae068f5a755d24023ba0fca4cb5
woodage <r-valkenburg@hotmail.com>
2017-01-19 13:17:25 +0100

    when char is not found throw exception

--- Source/Level.cpp
+++ Source/Level.cpp
@@ -13,6 +13,8 @@ ScreenObject * Level::screen_object_factory(char c, float x, float y) {
     if(c == '-') {
         return new Block(sf::Vector2f(x, y));
     }
+    std::string resultString = "\nUndifined char : ";
+    throw resultString + c;
 }
 
 std::vector<ScreenObject*> Level::get_screen_objects_from_map(std::string &map) {
@@ -27,14 +29,17 @@ std::vector<ScreenObject*> Level::get_screen_objects_from_map(std::string &map) 
     std::cout << input.fail();
     char c;
     while(input.get(c)) {
-        std::cout << "HELLO THERE !";
         // when new line..
         if(c == 10) {
             current_x = 0;
             current_y += 32;
             continue;
         } else if(c != '.') {
-            objects.push_back(screen_object_factory(c, current_x, current_y));
+            try {
+                objects.push_back(screen_object_factory(c, current_x, current_y));
+            } catch(std::string & exception) {
+                std::cout << exception;
+            }
         }
         current_x += 32;
     }
