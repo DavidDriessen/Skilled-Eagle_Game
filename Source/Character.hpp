fe6afd25f18427958e41176f6d8fa1d2b837f023
Max Beunk <mbeunk@live.nl>
2017-01-20 11:20:23 +0100

    begin of animation

--- Source/Character.hpp
+++ Source/Character.hpp
@@ -11,17 +11,24 @@
 class Character {
     private:
         sf::Vector2f position;
+        sf::Image img;
+        sf::Texture texture;
+        sf::Sprite sprite;
+        int sprite_state = 0;
+        float sprite_scale = 0.3;
+        int sprite_height = 260;
+        int sprite_width = 160;
         float gravity;
         float startHeight;
         float jumpHeigth;
         float speed;
         bool jumping = false;
         bool descending = false;
+        void move(sf::Vector2f direction);
+        void jump();
 
     public:
         Character(sf::Vector2f startPos, float gravity, float speed, float jumpHeight);
-        void move(sf::Vector2f direction);
-        void jump();
         void draw(sf::RenderWindow &window);
         void update(const sf::Time delta);
         void input(sf::Event &event);
