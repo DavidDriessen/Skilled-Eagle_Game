a884920d1e6c7517ed272030ff58af7e3db6ff30
woodage <r-valkenburg@hotmail.com>
2017-01-20 14:22:16 +0100

    Each object level get its own vector. All object will be place in level class. Also fixed a memory leak (deleting pointers first in list before cleaning).

--- Source/GameStates/PlayState.cpp
+++ Source/GameStates/PlayState.cpp
@@ -6,36 +6,28 @@
 #include <SFML/Graphics/RectangleShape.hpp>
 #include "PlayState.h"
 #include "../Game.h"
-PlayState::PlayState() {
 
+PlayState::PlayState(Game *pGame): level("assets/Levels/awesomeLevel.txt") {
+    game = pGame;
 }
 
-PlayState::PlayState(Game *pGame) {
-    game = pGame;
-    std::string mapName = "assets/Levels/awesomeLevel.txt";
-    level_objects = level.get_screen_objects_from_map(mapName);
+PlayState::~PlayState() {
+
 }
 
 void PlayState::input(sf::Event &event) {
     if(event.type == sf::Event::KeyPressed && sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
         game->go_to_menu();
     }
-    for(auto &object : level_objects) {
-        object->input(event);
-    }
 }
 
 void PlayState::update(const sf::Time delta) {
-    std::cout << "updating PlayState \n";
-    for(auto &object : level_objects) {
-        object->update(delta);
-    }
+
 }
 
 void PlayState::draw(sf::RenderWindow &window) {
-    std::cout << "drawing PlayState \n";
-    for(auto &object : level_objects) {
-        object->draw(window);
+    for(auto &block : level.get_blocks()) {
+        block->draw(window);
     }
 }
 
