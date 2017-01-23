d08b7fd19124583c6a1168620f979b8ee71941dd
Mike <m.hilhorst@hotmail.nl>
2017-01-23 16:30:23 +0100

    GAME-42 FMOD now windows and maybe linux compatible

--- Source/Audio/FmodApi.hpp
+++ Source/Audio/FmodApi.hpp
@@ -4,8 +4,7 @@
 
 #ifndef SKILLED_EAGLE_GAME_FMODAPI_HPP
 #define SKILLED_EAGLE_GAME_FMODAPI_HPP
-
-#include <fmod.hpp>
+#include <fmod.h>
 #include <cstdio>
 #include <stdlib.h>
 #include <fmod_errors.h>
@@ -17,18 +16,18 @@
 
 class FmodApi {
 private:
-    FMOD::System *system = NULL;
-    FMOD::Channel *channel = NULL;
-    FMOD::DSP *dsp = NULL;
+    FMOD_SYSTEM *system = NULL;
+    FMOD_CHANNEL *channel = NULL;
+    FMOD_DSP *dsp = NULL;
     float samples[2048];
     int peak = 0;
-    std::map<std::string, std::pair<FMOD::Channel*, FMOD::Sound*>> sounds;
+    std::map<std::string, std::pair<FMOD_CHANNEL*, FMOD_SOUND*>> sounds;
 public:
     FmodApi() {
-        has_error(FMOD::System_Create(&system));
-        has_error(system->init(512, FMOD_INIT_NORMAL, 0));
-        system->createDSPByType(FMOD_DSP_TYPE::FMOD_DSP_TYPE_FFT, &dsp);
-        dsp->setActive(true);
+        FMOD_System_Create(&system);
+        FMOD_System_Init(system, 32, FMOD_INIT_NORMAL, 0);
+        FMOD_System_CreateDSPByType(system, FMOD_DSP_TYPE::FMOD_DSP_TYPE_FFT, &dsp);
+        FMOD_DSP_SetActive(dsp, true);
     }
 
     void has_error(FMOD_RESULT res) {
@@ -39,37 +38,37 @@ public:
     }
 
     void add_song(const char * name, const char * path) {
-        FMOD::Sound *t;
-        FMOD::Channel *c;
+        FMOD_SOUND *t;
+        FMOD_CHANNEL *c;
         sounds.insert({name, {c, t}});
-        system->createStream(path, FMOD_DEFAULT, 0, &sounds.find(name)->second.second);
-        system->playSound(sounds.find(name)->second.second, 0, true, &sounds.find(name)->second.first);
+        FMOD_System_CreateStream(system, path, FMOD_DEFAULT, 0, &sounds.find(name)->second.second);
+        FMOD_System_PlaySound(system, sounds.find(name)->second.second, 0, false, &sounds.find(name)->second.first);
     }
 
     void play_song(const char *name) {
-        bool paused = NULL;
-        bool playing = NULL;
-        sounds.find(name)->second.first->getPaused(&paused);
-        sounds.find(name)->second.first->isPlaying(&playing);
+        FMOD_BOOL paused = false;
+        FMOD_BOOL playing = false;
+        FMOD_Channel_GetPaused(sounds.find(name)->second.first, &paused);
+        FMOD_Channel_IsPlaying(sounds.find(name)->second.first, &playing);
         if(!playing) {
-            system->playSound(sounds.find(name)->second.second, 0, false, &sounds.find(name)->second.first);
+            FMOD_System_PlaySound(system, sounds.find(name)->second.second, 0, false, &sounds.find(name)->second.first);
         }
         else if(paused) {
-            sounds.find(name)->second.first->setPaused(0);
+            FMOD_Channel_SetPaused(sounds.find(name)->second.first, false);
         } else {
-            system->playSound(sounds.find(name)->second.second, 0, false, &sounds.find(name)->second.first);
+            FMOD_System_PlaySound(system, sounds.find(name)->second.second, 0, false, &sounds.find(name)->second.first);
         }
-        sounds.find(name)->second.first->addDSP(FMOD_CHANNELCONTROL_DSP_HEAD, dsp);
+        FMOD_Channel_AddDSP(sounds.find(name)->second.first, FMOD_CHANNELCONTROL_DSP_HEAD, dsp);
     }
 
 
     void pause_song(const char *name) {
-        sounds.find(name)->second.first->setPaused(true);
+        FMOD_Channel_SetPaused(sounds.find(name)->second.first, true);
     }
 
     void toggle_play(const char *name) {
-        bool paused = NULL;
-        sounds.find(name)->second.first->getPaused(&paused);
+        FMOD_BOOL paused = false;
+        FMOD_Channel_GetPaused(sounds.find(name)->second.first, &paused);
         if(paused) {
             play_song(name);
         } else {
@@ -78,9 +77,9 @@ public:
     }
 
     void update() {
-        system->update();
+        FMOD_System_Update(system);
         FMOD_DSP_PARAMETER_FFT *fft;
-        dsp->getParameterData(FMOD_DSP_FFT_SPECTRUMDATA, (void **) &fft, 0, 0, 0);
+        FMOD_DSP_GetParameterData(dsp, FMOD_DSP_FFT_SPECTRUMDATA, (void **) &fft, 0, 0, 0);
         for (int bin = 0; bin < fft->length; bin++) {
             samples[bin] = fft->spectrum[0][bin];
         }
