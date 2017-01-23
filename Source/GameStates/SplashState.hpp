952661100f3e9e7de6d7a43edc29500c6ced5e4d
Mike <m.hilhorst@hotmail.nl>
2017-01-23 11:42:54 +0100

    Added extra splash screens

--- Source/GameStates/SplashState.hpp
+++ Source/GameStates/SplashState.hpp
@@ -8,17 +8,31 @@
 #include "IState.h"
 #include <SFML/Graphics.hpp>
 
+
 class Game;
 
 class SplashState : public IState {
 private:
     Game *game;
-    sf::Sprite sprite;
-    sf::Texture texture;
+    sf::Sprite sprite_eagle;
+    sf::Sprite sprite_name;
+    sf::Sprite sprite_presents;
+
+    sf::Texture texture_eagle;
+    sf::Texture texture_name;
+    sf::Texture texture_presents;
+
+    sf::Clock clock_eagle;
+    sf::Clock clock_name;
+    sf::Clock clock_presents;
+
     sf::Vector2f size;
-    sf::Clock clock;
     sf::Time elapsed;
-    sf::Time time_limit = sf::seconds(3);
+    sf::Time time_limit_name = sf::seconds(3);
+    sf::Time time_limit_presents = time_limit_name + sf::seconds(2);
+    sf::Time time_limit_eagle = time_limit_presents + sf::seconds(3);
+
+
 public:
     SplashState();
 
