60b59b9dbebf651ddfbc18f3b05c20b3d1ca461a
Jeremy Ruizenaar <jeremy.ruizenaar@student.hu.nl>
2017-01-25 15:54:45 +0100

    changed health to percentage healt, changed variable names, added statusbar* get, changed character constructor

--- Source/ScreenObjects/StatusBar.hpp
+++ Source/ScreenObjects/StatusBar.hpp
@@ -19,14 +19,17 @@ public:
     void input(sf::Event &event);
     void update(const sf::Time delta);
     void draw(sf::RenderWindow & window);
-    void set_StatusBar(int newCurrentStatus, sf::Vector2f position);
-
+    void set_StatusBar(float newCurrentStatus, sf::Vector2f position);
+    void set_StatusBar_hudSize();
+    float Calculate_percentage_of(float  newCurrentstatus ,float maximumOfStatus);
 private:
+
     std::string name;
     sf::Vector2f position;
     sf::Color color;
     sf::RectangleShape shape;
-    int currentStatus;
+    float currentStatus;
+    int  bar_height = 5;
 };
 
 
