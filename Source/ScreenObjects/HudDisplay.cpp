52c6573b077fa7aebf4a96836b962d90cb889bb3
Max Beunk <mbeunk@live.nl>
2017-02-01 11:46:03 +0100

    Created hud for powerup

--- Source/ScreenObjects/HudDisplay.cpp
+++ Source/ScreenObjects/HudDisplay.cpp
@@ -22,8 +22,13 @@ void HudDisplay::update(const sf::Time delta){
 void HudDisplay::draw(sf::RenderWindow &window) {
     Health->add_position(position + healthBarOffset);
     Stamina->add_position(position + staminaBarOffset);
+    powerup->setPosition(position + powerupOffset);
     Health->draw(window);
     Stamina->draw(window);
+    if(player->get_powerUp()){
+        window.draw(*powerup);
+    }
+
 }
 
 
@@ -36,7 +41,8 @@ void HudDisplay::set_hud_player(Character * newPlayer) {
     player = newPlayer;
     Health = player->get_HealthBar();
     Stamina = player->get_StaminaBar();
-
+    powerup->setSize(sf::Vector2f(64, 64));
+    powerup->setFillColor(sf::Color::Red);
 }
 
 void HudDisplay::set_Position(sf::Vector2f pos) {
