2de37502a9be6ac8393fb9ac6a8cfa30729d11f2
MSI\david <david.driessen@student.hu.nl>
2017-02-01 20:07:59 +0100

    Settings: Resolution

--- Source/GameStates/SettingsState.cpp
+++ Source/GameStates/SettingsState.cpp
@@ -21,8 +21,7 @@ SettingsState::SettingsState(Game *pGame) : Menu(pGame->getFonts().get(Fonts::De
         game->getSoundManager()->set_volume(volume / 100);
     });
     actions.push_back([&]() { game->go_to_controls(); });
-    actions.push_back([&]() { save(); oldVolume = volume; back(); });
-
+    actions.push_back([&]() { save(); back(); });
 
     std::string line;
     std::ifstream f("./assets/Settings/Settings.txt");
@@ -32,16 +31,19 @@ SettingsState::SettingsState(Game *pGame) : Menu(pGame->getFonts().get(Fonts::De
         std::stringstream linestream(line);
         std::string data;
         std::getline(linestream, data, '\n');
-        res = atoi(data.c_str());
+        oldres = res = atoi(data.c_str());
     }
     //volume
     if (std::getline(f, line)) {
         std::stringstream linestream(line);
         std::string data;
         std::getline(linestream, data, '\n');
-        volume = atoi(data.c_str());
+        oldVolume = volume = atoi(data.c_str());
     }
     f.close();
+//    game->get_window()->setSize(resV[res]);
+    game->get_window()->create(sf::VideoMode(resV[res].x, resV[res].y), "waveForms",
+                               (res == 0) ? sf::Style::Fullscreen : sf::Style::Default);
     game->getSoundManager()->set_volume(volume / 100);
 }
 
@@ -50,6 +52,7 @@ void SettingsState::input(sf::Event &event) {
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) { direction = -1; }
     if (event.type == sf::Event::KeyPressed && sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
         volume = oldVolume;
+        res = oldres;
         game->getSoundManager()->set_volume(volume / 100);
         back();
     }
@@ -74,8 +77,14 @@ void SettingsState::save() {
     std::ofstream f("./assets/Settings/Settings.txt");
     f.clear();
 
+//    game->get_window()->setSize(resV[res]);
+    game->get_window()->create(sf::VideoMode(resV[res].x, resV[res].y), "waveForms",
+                               (res == 0) ? sf::Style::Fullscreen : sf::Style::Default);
+    oldres = res;
     std::string resolution = std::to_string(res) + "\n";
     f.write(resolution.c_str(), resolution.size());
+
+    oldVolume = volume;
     std::string vol = std::to_string(volume) + "\n";
     f.write(vol.c_str(), vol.size());
 
